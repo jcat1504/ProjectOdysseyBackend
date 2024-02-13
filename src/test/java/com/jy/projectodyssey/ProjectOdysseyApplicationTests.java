c8fac854904ccb2ed35e85d97662063ec2e3d3e8
jcat1504 <jyin978@hotmail.com>
2024-02-12 22:06:33 -0500

    initial commit with project files

---
+++ src/test/java/com/jy/projectodyssey/ProjectOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.projectodyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
