b615be884d31b7fe9a69e60b7195c70743f01a19
jcat1504 <jyin978@hotmail.com>
2024-02-12 22:30:15 -0500

    test

--- src/main/java/com/jy/projectodyssey/ProjectOdysseyApplication.java
+++ src/main/java/com/jy/projectodyssey/ProjectOdysseyApplication.java
@@ -1,5 +1,4 @@
 package com.jy.projectodyssey;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectOdysseyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectOdysseyApplication.class, args);
+		System.out.println("This is a test!"); // Correct placement
 	}
+
 
 }
