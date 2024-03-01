8eef89eaec3594d748e08a40355632fb17675fb2
jcat1504 <jyin978@hotmail.com>
2024-02-29 20:11:43 -0500

    changes to service layer

---
+++ src/main/java/com/jy/projectodyssey/service/TaskService.java
@@ -0,0 +1,17 @@
+package com.jy.projectodyssey.service;
+
+import org.springframework.stereotype.Service;
+import com.jy.projectodyssey.model.Task;
+import java.util.List;
+@Service
+public class TaskService {
+    public List<Task> findAllTasks() {
+        return null;
+    }
+
+    public Task saveTask() {
+        return null;
+    }
+
+}
+
